3ab1473e173c33ef5ffdf2c083a0c75d5c2ed1cb
MursalHabib <mursal.habib27@gmail.com>
2024-09-02 11:09:38 +0700

    add config db prod

--- config/config.js
+++ config/config.js
@@ -16,10 +16,10 @@ module.exports = {
 		dialect: "mysql",
 	},
 	production: {
-		username: "root",
-		password: null,
-		database: "database_production",
-		host: "127.0.0.1",
-		dialect: "mysql",
+		username: "postgres",
+		password: "uljcvKNwUUbYLcBzQGAptiGHFUAYbblM",
+		database: "railway",
+		host: "meticulous-empathy.railway.internal",
+		dialect: "postgres",
 	},
 };
