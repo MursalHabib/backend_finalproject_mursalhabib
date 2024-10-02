e46b94a3c3dc0061c53d0c3e84e6b04861cab72a
MursalHabib <mursal.habib27@gmail.com>
2024-10-02 13:42:45 +0700

    configure dockerfile

--- db/models/index.js
+++ db/models/index.js
@@ -11,13 +11,19 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-	sequelize = new Sequelize(process.env[config.use_env_variable], config);
+	sequelize = new Sequelize(process.env[config.use_env_variable], {
+		...config,
+		dialect: "postgres",
+	});
 } else {
 	sequelize = new Sequelize(
 		config.database,
 		config.username,
 		config.password,
-		config
+		{
+			...config, // Spread the existing config object
+			dialect: "postgres", // Explicitly add the dialect for PostgreSQL
+		}
 	);
 }
 
